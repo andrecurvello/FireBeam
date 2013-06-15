d0e2de55988ecf013385f342c07b78c9cf9022c2
Joshua Hale <wildstang83@gmail.com>
2013-06-15 03:04:49 -0500

    Added color background to warning text

--- gen/com/ws83/firebeam/R.java
+++ gen/com/ws83/firebeam/R.java
@@ -13,7 +13,8 @@ public final class R {
     public static final class drawable {
         public static final int btn_switch_off=0x7f020000;
         public static final int btn_switch_on=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int button=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int btnSwitch=0x7f050003;
